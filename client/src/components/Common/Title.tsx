a3a0e082cc45aa9720acfc8454f703838e8c9147
RyxnDmello <ryanndmello10@gmail.com>
2024-06-16 11:43:46 +0530

    Added Responsiveness

--- client/src/components/Common/Title.tsx
+++ client/src/components/Common/Title.tsx
@@ -2,7 +2,7 @@ import styles from "./Title.module.scss";
 
 export default function Title({ primary, secondary, position }: ITitle) {
   return (
-    <h1 className={`${styles.title} ${position}`}>
+    <h1 className={`${styles.title} ${position} title`}>
       <span>{primary}</span> {secondary}
     </h1>
   );
