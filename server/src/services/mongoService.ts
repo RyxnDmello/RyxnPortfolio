164e57c5479c8d149098e29b34e69fee3257d8f4
RyxnDmello <ryanndmello10@gmail.com>
2024-11-19 02:24:17 +0530

    update(database): improve mongodb authentication

--- server/src/services/mongoService.ts
+++ server/src/services/mongoService.ts
@@ -1,17 +1,29 @@
 import dotenv from "dotenv";
-import { connect } from "mongoose";
+import { connection, connect, disconnect } from "mongoose";
 
 dotenv.config();
 
 const connectMongoDB = async () => {
   try {
     await connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_DB}`, {
+      serverApi: {
+        deprecationErrors: true,
+        strict: true,
+        version: "1",
+      },
       appName: process.env.MONGODB_APP,
       retryWrites: true,
       retryReads: true,
     });
+
+    if (!connection.db) {
+      throw new Error("MongoDB Connection Failed");
+    }
+
+    await connection.db.admin().ping();
   } catch (error) {
     console.log(`MongoDB Error: ${error instanceof Error && error.message}`);
+    await disconnect();
     process.exit();
   }
 };
